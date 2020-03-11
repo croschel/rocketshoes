7d78ba111404b39c0bfcb31d214fdf8bbcb4a411
croschel <croschel000@gmail.com>
2020-03-10 22:09:40 -0300

    The styles are Ready with a simple Flex Layout

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,74 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return (
-    <h1>Cart</h1>
-  );
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
+                                alt="shoes"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tênis muito maneiro</strong>
+                            <span>R$129,99</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={1} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$259,98</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar Pedido</button>
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+import {formatPrice} from '../../util/format';
+import api from '../../services/api';
+import { ProductList } from './styles';
 
-// import { Container } from './styles';
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
 
-export default function Home() {
-  return (
-    <h1>Home</h1>
-  );
+    async componentDidMount() {
+        const response = await api.get('products');
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }))
+        this.setState({ products: data });
+    }
+
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map(produto => (
+                    <li>
+                        <img src={produto.image} alt={produto.title} />
+                        <strong>{produto.title}</strong>
+                        <span>{produto.priceFormatted}</span>
+                        <button type="button">
+                            <div>
+                                <MdShoppingCart size={16} color="#FFF" />
+                            </div>
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
