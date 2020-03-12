7719b980deb6f94f7ef12aad96ccd6b0f4d1ed22
croschel <croschel000@gmail.com>
2020-03-12 11:21:44 -0300

    rocketshoes using Redux to update cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,15 +1,24 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
   MdDelete,
 } from "react-icons/md";
-
 import { Container, ProductTable, Total } from "./styles";
+import * as cartActions from "../../store/modules/cart/actions";
+import { formatPrice } from "../../util/format";
 
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart, updateAmount, total }) {
+  function incrementAmount(product) {
+    updateAmount(product.id, product.amount + 1);
+  }
+  function decrementAmount(product) {
+    updateAmount(product.id, product.amount - 1);
+  }
+
   return (
     <Container>
       <ProductTable>
@@ -34,24 +43,28 @@ function Cart({ cart, dispatch }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button
+                    type="button"
+                    onClick={() => decrementAmount(product)}
+                  >
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
                   <input type="number" readOnly value={product.amount} />
-                  <button type="button">
+                  <button
+                    type="button"
+                    onClick={() => incrementAmount(product)}
+                  >
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
                 </div>
               </td>
               <td>
-                <strong>R$259,98</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                    dispatch({ type: "REMOVE_FROM_CART", id: product.id })
-                  }
+                  onClick={() => removeFromCart(product.id)}
                 >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
@@ -65,7 +78,7 @@ function Cart({ cart, dispatch }) {
         <button type="button">Finalizar Pedido</button>
         <Total>
           <span>TOTAL</span>
-          <strong>R$1920,28</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -73,7 +86,18 @@ function Cart({ cart, dispatch }) {
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(cartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import { MdShoppingCart } from "react-icons/md";
 import { formatPrice } from "../../util/format";
 import api from "../../services/api";
 import { ProductList } from "./styles";
+import * as cartActions from "../../store/modules/cart/actions";
 
 class Home extends Component {
   state = {
@@ -21,15 +23,13 @@ class Home extends Component {
   }
 
   handleAddShoesToCart = product => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: "ADD_TO_CART",
-      product,
-    });
+    const { addToCart } = this.props;
+    addToCart(product);
   };
 
   render() {
     const { products } = this.state;
+    const { amountHome } = this.props;
     return (
       <ProductList>
         {products.map(product => (
@@ -43,6 +43,7 @@ class Home extends Component {
             >
               <div>
                 <MdShoppingCart size={16} color="#FFF" />
+                {amountHome[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -52,5 +53,14 @@ class Home extends Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  amountHome: state.cart.reduce((amountHome, product) => {
+    amountHome[product.id] = product.amount;
+    return amountHome;
+  }, {}),
+});
 
-export default connect()(Home);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(cartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
